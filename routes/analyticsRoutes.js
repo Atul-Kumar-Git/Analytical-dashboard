5b7dc90341f87d8afd7c2d0421848b20e201ac17
Atul-Kumar-Git <atul9abc@gmail.com>
2026-04-18 09:38:48 +0530

    Delete routes directory

--- routes/analyticsRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getAnalytics } = require('../controllers/analyticsController');
-const authMiddleware = require('../middleware/authMiddleware');
-
-router.post('/', authMiddleware, getAnalytics);
-
-module.exports = router;
--- routes/trackRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { track } = require('../controllers/trackController');
-const authMiddleware = require('../middleware/authMiddleware');
-
-router.post('/', authMiddleware, track);
-
-module.exports = router;
